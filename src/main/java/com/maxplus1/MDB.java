c41ac420cce589abb8d5fd7b664247d53234ae9a
qiuxiaolong <qiuxiaolong@cmschina.com.cn>
2018-11-13 20:47:36 +0800

    add

---
+++ src/main/java/com/maxplus1/MDB.java
@@ -0,0 +1,54 @@
+package com.maxplus1;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * 描述：
+ * @since 1.9.10
+ * @version 1.9.10
+ * @作者：niexiaohui
+ * @创建时间：2016年11月22日
+ * @修改记录：
+ */
+public class MDB {
+    public static void main(String[] args) throws URISyntaxException {
+        try {
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            InputStream is = classloader.getResourceAsStream("generatorConfig.xml");
+            Properties properties = new Properties();
+            InputStream propIs = classloader.getResourceAsStream("mdb.properties");
+            properties.load(propIs);
+            ConfigurationParser cp = new ConfigurationParser(properties,warnings);
+            Configuration config = cp.parseConfiguration(is);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
